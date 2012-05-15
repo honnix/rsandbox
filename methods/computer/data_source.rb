79f2a52475bfe5a0fb835cb64cb1db955005f797
ehonlia <hongxin.liang@ericsson.com>
2012-05-15 13:50:53 +0800

    the initial duplicated version

---
+++ methods/computer/data_source.rb
@@ -0,0 +1,69 @@
+class DS
+  def initialize
+    
+  end
+
+  def get_mouse_info(workstation_id)
+    
+  end
+
+  def get_mouse_price(workstation_id)
+    
+  end
+
+  def get_keyboard_info(workstation_id)
+    
+  end
+
+  def get_keyboard_price(workstation_id)
+    
+  end
+
+  def get_cpu_info(workstation_id)
+    
+  end
+
+  def get_cpu_price(workstation_id)
+    
+  end
+end
+
+class Computer
+  def initialize(computer_id, data_source)
+    @id = computer_id
+    @data_source = data_source
+  end
+
+  def mouse
+    info = @data_source.get_mouse_info(@id)
+    price = @data_source.get_mouse_price(@id)
+    result = "Mouse: #{info} ($#{price})"
+    if price >= 100
+      "* #{result}"
+    else
+      result
+    end
+  end
+
+  def cpu
+    info = @data_source.get_cpu_info(@id)
+    price = @data_source.get_cpu_price(@id)
+    result = "Cpu: #{info} ($#{price})"
+    if price >= 100
+      "* #{result}"
+    else
+      result
+    end
+  end
+
+  def keyboard
+    info = @data_source.get_keyboard_info(@id)
+    price = @data_source.get_keyboard_price(@id)
+    result = "Keyboard: #{info} ($#{price})"
+    if price >= 100
+      "* #{result}"
+    else
+      result
+    end
+  end
+end
