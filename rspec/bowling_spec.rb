8a52f26ee40ed87358670f91e68e9b25dfca5db2
ehonlia <hongxin.liang@ericsson.com>
2012-06-01 14:27:08 +0800

    rspec

---
+++ rspec/bowling_spec.rb
@@ -0,0 +1,11 @@
+$: << File.dirname(__FILE__)
+
+require 'bowling'
+
+describe Bowling, '#score' do
+  it 'returns 0 for all gutter game' do
+    bowling = Bowling.new
+    20.times { bowling.hit(0) }
+    bowling.score.should eq(0)
+  end
+end
